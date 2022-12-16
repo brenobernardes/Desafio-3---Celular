5c5bde11fb2c3321a71d5c9af400b10a7e709209
Breno Bernardes <breno.pbernardes@gmail.com>
2022-12-16 19:21:09 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using DesafioPOO.Models;
+
+Iphone iphone = new Iphone ("123456", "X-pro", "987654321", 128);
+
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("whatsapp");
+
+Nokia nokia = new Nokia("78955130", "3310", "56449916", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("instagram");
